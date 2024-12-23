8077ea4cfa2bdd52732aa077c09eeea053a8eb48
Aziz Azizli <azizliaziz025@gmail.com>
2024-12-23 20:13:52 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </>
   );
--- src/components/BarGraph/BarGraph.jsx
+++ src/components/BarGraph/BarGraph.jsx
@@ -2,57 +2,74 @@ import React, { useEffect, useState } from 'react'
 import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip,Legend} from 'chart.js';
 import { Bar } from 'react-chartjs-2'
 import { fetchData } from '../../services/fetch';
-const data = {
-    labels: ['Jun', 'Jul', 'Aug', "test2", "test3", "test4"],
-    datasets: [
-        {
-            id: 1,
-            label: '',
-            data: [5, 7, 2, 4, 5, 3],
-            borderColor: "rgba(0,0,0)",
-            backgroundColor: 'rgba(0,0,0)',
-        },
-        {
-            id: 1,
-            label: 'steps',
-            data: [1, 4, 7, 6, 7, 3],
-            borderColor: "rgba(0,0,0)",
-            backgroundColor: 'rgba(0,0,255)',
+// const data = {
+//     labels: [
+//         "January", 
+//         "February", 
+//         "March", 
+//         "April", 
+//         "May", 
+//         "June", 
+//         "July", 
+//         "August", 
+//         "September", 
+//         "October", 
+//         "November", 
+//         "December"],
+//     datasets: [
+//         {
+//             id: 1,
+//             label: 'Revenue',
+//             data: [5, 7, 2, 4, 5, 8,5,7,3,6,2,4],
+//             borderColor: "rgba(0,0,0)",
+//             backgroundColor: 'rgba(0,0,0)',
+//         },
+//         {
+//             id: 2,
+//             label: 'Revenue',
+//             data: [1, 4, 7, 6, 7, 3,7,4,3,8,2,5],
+//             borderColor: "rgba(0,0,0)",
+//             backgroundColor: 'rgba(0,0,255)',
             
-        },
+//         },
 
-    ],
-};
-const months = [
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"
-  ];
+//     ],
+// };
+// const months = [
+//     "January", 
+//     "February", 
+//     "March", 
+//     "April", 
+//     "May", 
+//     "June", 
+//     "July", 
+//     "August", 
+//     "September", 
+//     "October", 
+//     "November", 
+//     "December"
+//   ];
   
-const mock = [1, 2, 4, 3, 7, 1, 6];
+// const mock = [1, 2, 4, 3, 7, 1, 6];
 const options = {
     responsive:true
 };
 const BarGraph = () => {
     ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
     
-    const [chartData, setChartData] = useState({});
+    const [chartData, setChartData] = useState({labels:[],datasets:[]});
 
 
     async function getData() {
-        const resData = await fetchData('https://b35c-85-132-107-241.ngrok-free.app/data', "GET", "");
-        console.log(resData);
-        
+        const resData = await fetchData('http://10.121.128.76:8000/data', "GET");
+        // console.log(resData.data);
+        if (resData.status === 200) { 
+            setChartData(resData?.data);
+        }
+        return
     }
+
+
     useEffect(() => {
         getData();
     },[])
@@ -61,7 +78,7 @@ const BarGraph = () => {
        
   return (
       <div style={{width:"50%"}}>
-          <Bar options={options} data={data}/>
+          <Bar options={options} data={chartData}/>
     </div>
   )
 }
