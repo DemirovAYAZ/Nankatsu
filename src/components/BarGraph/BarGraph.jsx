ee9558594b2683b17f5cbde29a00a833b3ec56bb
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2024-12-23 21:36:48 +0400

    changes

--- src/components/BarGraph/BarGraph.jsx
+++ src/components/BarGraph/BarGraph.jsx
@@ -2,39 +2,39 @@ import React, { useEffect, useState } from 'react'
 import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip,Legend} from 'chart.js';
 import { Bar } from 'react-chartjs-2'
 import { fetchData } from '../../services/fetch';
-// const data = {
-//     labels: [
-//         "January", 
-//         "February", 
-//         "March", 
-//         "April", 
-//         "May", 
-//         "June", 
-//         "July", 
-//         "August", 
-//         "September", 
-//         "October", 
-//         "November", 
-//         "December"],
-//     datasets: [
-//         {
-//             id: 1,
-//             label: 'Revenue',
-//             data: [5, 7, 2, 4, 5, 8,5,7,3,6,2,4],
-//             borderColor: "rgba(0,0,0)",
-//             backgroundColor: 'rgba(0,0,0)',
-//         },
-//         {
-//             id: 2,
-//             label: 'Revenue',
-//             data: [1, 4, 7, 6, 7, 3,7,4,3,8,2,5],
-//             borderColor: "rgba(0,0,0)",
-//             backgroundColor: 'rgba(0,0,255)',
+const data = {
+    labels: [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June", 
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"],
+    datasets: [
+        {
+            id: 1,
+            label: 'Revenue',
+            data: [5, 7, 2, 4, 5, 8,5,7,3,6,2,4],
+            borderColor: "rgba(0,0,0)",
+            backgroundColor: 'rgba(0,0,0)',
+        },
+        {
+            id: 2,
+            label: 'Revenue',
+            data: [1, 4, 7, 6, 7, 3,7,4,3,8,2,5],
+            borderColor: "rgba(0,0,0)",
+            backgroundColor: 'rgba(0,0,255)',
             
-//         },
+        },
 
-//     ],
-// };
+    ],
+};
 // const months = [
 //     "January", 
 //     "February", 
@@ -78,7 +78,7 @@ const BarGraph = () => {
        
   return (
       <div >
-          <Bar options={options} data={chartData}/>
+          <Bar options={options} data={data}/>
     </div>
   )
 }
